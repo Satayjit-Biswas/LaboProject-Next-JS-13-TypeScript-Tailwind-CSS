662eced33942e59773c36d6aa6943851b09840fd
Satayjit-Biswas <satayjitbiswas2@gmail.com>
2023-10-09 22:01:27 +0600

    main Header design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import HeaderMain from "@/components/HeaderMain";
 import HeaderTop from "@/components/HeaderTop";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -19,6 +20,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<HeaderTop></HeaderTop>
+				<HeaderMain></HeaderMain>
 				{children}
 			</body>
 		</html>
