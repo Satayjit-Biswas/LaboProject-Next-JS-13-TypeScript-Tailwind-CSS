5312dbf91fa19126287839feb1ab7accaaf00a3a
Satayjit-Biswas <satayjitbiswas2@gmail.com>
2023-10-17 16:23:23 +0600

    fixed menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,28 +5,28 @@ const Navbar = () => {
 		<div className="hidden lg:block">
 			<div className="container">
 				<div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
-					<Link className="navbar__link relative" href="#">
+					<Link className="navbar__link" href="#">
 						HOME
 					</Link>
-					<Link className="navbar__link relative" href="#">
+					<Link className="navbar__link" href="#">
 						CATEGORIES
 					</Link>
-					<Link className="navbar__link relative" href="#">
+					<Link className="navbar__link" href="#">
 						{`MEN'S`}
 					</Link>
-					<Link className="navbar__link relative" href="#">
+					<Link className="navbar__link" href="#">
 						{`WOMEN'S`}
 					</Link>
-					<Link className="navbar__link relative" href="#">
+					<Link className="navbar__link" href="#">
 						JEWELRY
 					</Link>
-					<Link className="navbar__link relative" href="#">
+					<Link className="navbar__link" href="#">
 						PERFUME
 					</Link>
-					<Link className="navbar__link relative" href="#">
+					<Link className="navbar__link" href="#">
 						BLOG
 					</Link>
-					<Link className="navbar__link relative" href="#">
+					<Link className="navbar__link" href="#">
 						HOT OFFERS
 					</Link>
 				</div>
